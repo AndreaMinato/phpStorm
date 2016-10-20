1a299439cb5725bea290fbcfde051d83de22648e
AndreaMinato <andrea.minato@hotmail.it>
2016-10-20 11:44:23 +0200

    file eliminati

--- scam/scriptAppunti.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    $( "#inserito" ).datepicker();
-    $( "#inserito" ).on('change', function() {
-        $( "#inserito" ).datepicker( "option", "dateFormat", "yy-mm-dd" );
-    });
-});
